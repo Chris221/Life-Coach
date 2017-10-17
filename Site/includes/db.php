56c2d0c8295ff36676b72fc1127b2153c4bf3f81
Chris Siena <zenatron@gmail.com>
2017-10-17 18:33:03 -0400

    more goodies to know

--- Site/includes/db.php
+++ Site/includes/db.php
@@ -7,8 +7,14 @@
 
 	//CODE FOR LATER USE, Love past Chris
 	//$result = pg_query($conn, "SQL GOES HERE");
-	//while ($row = pg_fetch_row($result)) 
+	//while ($row = pg_fetch_row($result))
 	//pg_fetch_all($result);
 	//pg_escape_string($conn, "STRING GOES HERE");
+	//pg_last_error($conn);
 	//pg_close($conn);
+
+	//Get last insert id
+    //$insert_query = pg_query($conn,"SELECT lastval();");
+	//$insert_row = pg_fetch_row($insert_query);
+	//$last_insert_id = $insert_row[0];
 ?>
