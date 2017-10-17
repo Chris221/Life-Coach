dafb8b6d072fe6f84e657fce4f855e8c68373e03
Chris Siena <zenatron@gmail.com>
2017-10-17 18:33:15 -0400

    mail script

---
+++ Site/includes/mailer.php
@@ -0,0 +1,19 @@
+<?php
+	function my_mailer($email, $subject, $body) {
+		$_SESSION['companyid'];
+		include('includes/db.php');
+		$sql = "SELECT domain FROM companies WHERE companyid='$companyid'";
+		$data = pg_query($conn, $sql);
+		$data = pg_fetch_all($data);
+		pg_close($conn);
+		
+		$from = 'system@'.$data['domain'];
+		$headers = '';
+		$headers .= 'MIME-Version: 1.0'."\r\n";
+		$headers .= 'Content-type: text/html; charset=iso-8859-1'."\r\n";
+		$headers .= 'From: '.$from."\r\n";
+		$headers .= 'Return-Path: '.$from."\r\n";
+		$headers .=	'X-Mailer: PHP/' . phpversion();
+		return mail($email, $subject, '<html><head><title>'.$subject.'</title></head><body>'.$body.'</body></html>', $headers, '-f'.$from);
+	}
+?>
