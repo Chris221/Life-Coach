5038800e7e83bde037b06013587dd99feaad8d11
Chris Siena <zenatron@gmail.com>
2017-11-01 10:15:36 -0400

    removed the first slash in the css and js references
    The prior was causing it to reference the site file not the local in editors

--- Site/Login.php
+++ Site/Login.php
@@ -98,13 +98,13 @@
 <meta name="viewport" content="width=device-width, user-scalable=no" />
 <meta name="HandheldFriendly" content="true">
 <!-- Latest compiled and minified CSS -->
-<link rel="stylesheet" href="/bootstrap/4.0.0/css/bootstrap.min.css">
+<link rel="stylesheet" href="bootstrap/4.0.0/css/bootstrap.min.css">
 <!-- jQuery library -->
-<script src="/js/jquery/jquery-3.2.1.min.js"></script>
+<script src="js/jquery/jquery-3.2.1.min.js"></script>
 <!-- Latest compiled JavaScript -->
-<script src="/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+<script src="bootstrap/4.0.0/js/bootstrap.min.js"></script>
 <!-- Our CSS -->
-<link rel="stylesheet" href="/css/life-coach.css">
+<link rel="stylesheet" href="css/life-coach.css">
 <title><?php echo($title); ?></title>
 </head>
 <body>
