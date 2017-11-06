914bd71a36147444484a540a704b5c7e2ffaea7d
mproscia <marisa.proscia1@marist.edu>
2017-11-05 20:23:25 -0500

    Added small calendar to home page

--- Site/index.php
+++ Site/index.php
@@ -105,7 +105,63 @@
             <div class="card text-center page-margin5 right">
                 <div class="card-header title"> Calendar</div>
                 <div class="card-body">
-                    <h5 class="card-title">A calendar will appear here.</h5>
+                    <div class="month roundCornerTop">
+                        <ul>
+                            <li class="prev">&#10094;</li>
+                            <li class="next">&#10095;</li>
+                            <li>
+                                November<br>
+                                <span style="font-size:18px">2017</span>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="calendar">
+                        <ul class="weekdays">
+                            <li>Mo</li>
+                            <li>Tu</li>
+                            <li>We</li>
+                            <li>Th</li>
+                            <li>Fr</li>
+                            <li>Sa</li>
+                            <li>Su</li>
+                        </ul>
+
+                        <ul class="days roundCornerBottom">
+                            <li>  </li>
+                            <li>  </li>
+                            <li>  </li>
+                            <li>1</li>
+                            <li>2</li>
+                            <li>3</li>
+                            <li>4</li>
+                            <li>5</li>
+                            <li>6</li>
+                            <li>7</li>
+                            <li><span class="active">8</span></li>
+                            <li>9</li>
+                            <li>10</li>
+                            <li>11</li>
+                            <li>12</li>
+                            <li>13</li>
+                            <li>14</li>
+                            <li>15</li>
+                            <li>16</li>
+                            <li>17</li>
+                            <li>18</li>
+                            <li>19</li>
+                            <li>20</li>
+                            <li>21</li>
+                            <li>22</li>
+                            <li>23</li>
+                            <li>24</li>
+                            <li>25</li>
+                            <li>26</li>
+                            <li>27</li>
+                            <li>28</li>
+                            <li>29</li>
+                            <li>30</li>
+                        </ul>
+                    </div>
                 </div>
             </div>
 
