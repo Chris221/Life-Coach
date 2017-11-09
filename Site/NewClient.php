79cb2967afb88cdeeda2206a925b58a0ace96f5b
Chris Siena <zenatron@gmail.com>
2017-11-08 19:02:26 -0500

    corrected indent in PHP

--- Site/NewClient.php
+++ Site/NewClient.php
@@ -1,135 +1,136 @@
 <?php
-include('includes/log.php');
-include('includes/session.php');
-include('includes/uploadPhoto.php');
-if (!$_SESSION['employeed']) {
-    header('Location: /');
-}
-o_log('Page Loaded');
-$title = 'Add New Client';
+	include('includes/log.php');
+	include('includes/session.php');
+	include('includes/uploadPhoto.php');
+			include('includes/api.php');
+	if (!$_SESSION['employeed']) {
+		header('Location: /');
+	}
+	o_log('Page Loaded');
+	$title = 'Add New Client';
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    include('includes/api.php');
-    include('includes/db.php');
-    $prefix = pg_escape_string($conn, $_POST['prefix']);
-    $firstname = pg_escape_string($conn, $_POST['firstname']);
-    $middlename = pg_escape_string($conn, $_POST['middlename']);
-    $lastname = pg_escape_string($conn, $_POST['lastname']);
-    $suffix = pg_escape_string($conn, $_POST['suffix']);
-    $email1 = pg_escape_string($conn, strtolower($_POST['email1']));
-    $email2 = pg_escape_string($conn, strtolower($_POST['email2']));
-    $cell = cleanPhoneNumber($_POST['cell']);
-    $home = cleanPhoneNumber($_POST['home']);
-    $worknumber = cleanPhoneNumber($_POST['work']);
-    $extension = cleanPhoneNumber($_POST['extension']);
-    $dob = $_POST['dob'];
 
-    $workcompany = $_POST['workcompany'];
-    $worktitle = $_POST['worktitle'];
-    $workfield = $_POST['workfield'];
-    $visitpreferencestart = checkTime($_POST['visittimepreferencestart'] . ':00');
-    $visitpreferenceend = checkTime($_POST['visittimepreferenceend'] . ':00');
-    $callpreferencestart = checkTime($_POST['calltimepreferencestart'] . ':00');
-    $callpreferenceend = checkTime($_POST['calltimepreferenceend'] . ':00');
-    $favoritebook = $_POST['favoritebook'];
-    $favoritefood = $_POST['favoritefood'];
-    $goals = $_POST['goals'];
-    $needs = $_POST['needs'];
+	if ($_SERVER["REQUEST_METHOD"] == "POST") {
+		include('includes/db.php');
+		$prefix = pg_escape_string($conn, $_POST['prefix']);
+		$firstname = pg_escape_string($conn, $_POST['firstname']);
+		$middlename = pg_escape_string($conn, $_POST['middlename']);
+		$lastname = pg_escape_string($conn, $_POST['lastname']);
+		$suffix = pg_escape_string($conn, $_POST['suffix']);
+		$email1 = pg_escape_string($conn, strtolower($_POST['email1']));
+		$email2 = pg_escape_string($conn, strtolower($_POST['email2']));
+		$cell = cleanPhoneNumber($_POST['cell']);
+		$home = cleanPhoneNumber($_POST['home']);
+		$worknumber = cleanPhoneNumber($_POST['work']);
+		$extension = cleanPhoneNumber($_POST['extension']);
+		$dob = $_POST['dob'];
 
-    pg_close($conn);
+		$workcompany = $_POST['workcompany'];
+		$worktitle = $_POST['worktitle'];
+		$workfield = $_POST['workfield'];
+		$visitpreferencestart = checkTime($_POST['visittimepreferencestart'] . ':00');
+		$visitpreferenceend = checkTime($_POST['visittimepreferenceend'] . ':00');
+		$callpreferencestart = checkTime($_POST['calltimepreferencestart'] . ':00');
+		$callpreferenceend = checkTime($_POST['calltimepreferenceend'] . ':00');
+		$favoritebook = $_POST['favoritebook'];
+		$favoritefood = $_POST['favoritefood'];
+		$goals = $_POST['goals'];
+		$needs = $_POST['needs'];
 
-    $work = true;
-    if (strlen($email1) < 1) {
-        $text = "The Email Adress cannot be empty.<br />";
-        $work = false;
-    }
-    if (strlen($firstname) < 1) {
-        $text .= "First name cannot be blank.<br />";
-        $work = false;
-    }
-    if (strlen($lastname) < 1) {
-        $text .= "Last name cannot be blank.<br />";
-        $work = false;
-    }
-    if (strlen($cell) < 1) {
-        $text .= "Cell number cannot be blank.<br />";
-        $work = false;
-    }
+		pg_close($conn);
 
-    if (strlen($email1) > 100) {
-        $text .= "The Email Adress is to long.<br />";
-        $work = false;
-    }
-    if (strlen($firstname) > 50) {
-        $text .= "The First name is to long.<br />";
-        $work = false;
-    }
-    if (strlen($lastname) > 50) {
-        $text .= "The Last name cannot is to long.<br />";
-        $work = false;
-    }
-    if (strlen($cell) < 9) {
-        $text .= "Cell number cannot be less then 9 digets.<br />";
-        $work = false;
-    }
-    if (strlen($cell) > 15) {
-        $text .= "Cell number cannot be longer then 15 digets.<br />";
-        $work = false;
-    }
+		$work = true;
+		if (strlen($email1) < 1) {
+			$text = "The Email Adress cannot be empty.<br />";
+			$work = false;
+		}
+		if (strlen($firstname) < 1) {
+			$text .= "First name cannot be blank.<br />";
+			$work = false;
+		}
+		if (strlen($lastname) < 1) {
+			$text .= "Last name cannot be blank.<br />";
+			$work = false;
+		}
+		if (strlen($cell) < 1) {
+			$text .= "Cell number cannot be blank.<br />";
+			$work = false;
+		}
 
-    // Check for valid email
-    if (!filter_var($email1, FILTER_VALIDATE_EMAIL) === false) {
-        //echo("$email is a valid email address.");
-    } else if ($work) {
-        $text = "\"$email1\" is not a valid email address.<br />";
-        $work = false;
-    }
-    // Check email equality
-    if ($email1 === $email2) {
-        //echo("The emails match.");
-    } else if ($work) {
-        $text = "The emails do not match.<br />";
-        $work = false;
-    }
+		if (strlen($email1) > 100) {
+			$text .= "The Email Adress is to long.<br />";
+			$work = false;
+		}
+		if (strlen($firstname) > 50) {
+			$text .= "The First name is to long.<br />";
+			$work = false;
+		}
+		if (strlen($lastname) > 50) {
+			$text .= "The Last name cannot is to long.<br />";
+			$work = false;
+		}
+		if (strlen($cell) < 9) {
+			$text .= "Cell number cannot be less then 9 digets.<br />";
+			$work = false;
+		}
+		if (strlen($cell) > 15) {
+			$text .= "Cell number cannot be longer then 15 digets.<br />";
+			$work = false;
+		}
 
-    if ($work) {
-        // Check for Email duplicates
-        $data = view('accounts', "email='$email1'", true);
-        if ($data['personid']) {
-            $text = "The email address \"$email1\" already exists.<br />";
-            $work = false;
-        }
-    }
-    if ($work) {
-        //$photoid = uploadImage();
-        $email1 = strtolower($email1);
-        $correctDOB = date("Y-m-d", strtotime($dob));
-        $companyid = $_SESSION['companyid'];
-        $pid = addPerson($firstname, $lastname, $email1, $cell, $companyid, $photoid, $prefix, $suffix, $home, $worknumber, $extension, $correctDOB, $address, $middlename);
-        $output = true;
-        if ($pid && $output) {
-            echo("Person was added succesfully!<br />");
-            echo("Person ID:" . $pid . "<br />");
-            o_log('Person Add Successful', 'ID: ' . $pid);
-        } else if ($output) {
-            echo("ERROR PERSON WAS NOT ADDED!<br />");
-            o_log('Person Add failed');
-        }
+		// Check for valid email
+		if (!filter_var($email1, FILTER_VALIDATE_EMAIL) === false) {
+			//echo("$email is a valid email address.");
+		} else if ($work) {
+			$text = "\"$email1\" is not a valid email address.<br />";
+			$work = false;
+		}
+		// Check email equality
+		if ($email1 === $email2) {
+			//echo("The emails match.");
+		} else if ($work) {
+			$text = "The emails do not match.<br />";
+			$work = false;
+		}
 
-        $coachid = $_SESSION['coachid'];
+		if ($work) {
+			// Check for Email duplicates
+			$data = view('accounts', "email='$email1'", true);
+			if ($data['personid']) {
+				$text = "The email address \"$email1\" already exists.<br />";
+				$work = false;
+			}
+		}
+		if ($work) {
+			//$photoid = uploadImage();
+			$email1 = strtolower($email1);
+			$correctDOB = date("Y-m-d", strtotime($dob));
+			$companyid = $_SESSION['companyid'];
+			$pid = addPerson($firstname, $lastname, $email1, $cell, $companyid, $photoid, $prefix, $suffix, $home, $worknumber, $extension, $correctDOB, $address, $middlename);
+			$output = true;
+			if ($pid && $output) {
+				echo("Person was added succesfully!<br />");
+				echo("Person ID:" . $pid . "<br />");
+				o_log('Person Add Successful', 'ID: ' . $pid);
+			} else if ($output) {
+				echo("ERROR PERSON WAS NOT ADDED!<br />");
+				o_log('Person Add failed');
+			}
 
-        $cid = addClient($pid, $workaddress, $workcompany, $worktitle, $workfield, $favoritebook, $favoritefood, $visitpreferencestart, $visitpreferenceend, $callpreferencestart, $callpreferenceend, $goals, $needs, $coachid);
-        if ($cid && $output) {
-            echo("Client was added succesfully!<br />");
-            echo("Client ID:" . $cid . "<br />");
-            o_log('Client Add Successful', 'ID: ' . $cid);
-            header('Location: /');
-        } else if ($output) {
-            echo("ERROR CLIENT WAS NOT ADDED!<br />");
-            o_log('Client Add failed');
-        }
-    }
-}
+			$coachid = $_SESSION['coachid'];
+
+			$cid = addClient($pid, $workaddress, $workcompany, $worktitle, $workfield, $favoritebook, $favoritefood, $visitpreferencestart, $visitpreferenceend, $callpreferencestart, $callpreferenceend, $goals, $needs, $coachid);
+			if ($cid && $output) {
+				echo("Client was added succesfully!<br />");
+				echo("Client ID:" . $cid . "<br />");
+				o_log('Client Add Successful', 'ID: ' . $cid);
+				header('Location: /');
+			} else if ($output) {
+				echo("ERROR CLIENT WAS NOT ADDED!<br />");
+				o_log('Client Add failed');
+			}
+		}
+	}
 ?>
 <!doctype html>
 <html>
